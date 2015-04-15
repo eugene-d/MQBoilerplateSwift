43de8b399a22b44745c115aec1b1dc3acf563c75
Matt Quiros <mattquiros@gmail.com>
2015-04-15 11:24:37 +0800

    Initial commit

---
+++ MQBoilerplateSwift/Extensions/UIView+MQBoilerplateExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+MQBoilerplateExtensions.swift
+//  MQBoilerplateSwift
+//
+//  Created by Matt Quiros on 4/15/15.
+//  Copyright (c) 2015 Matt Quiros. All rights reserved.
+//
+
+import Foundation
+
+public extension UIView {
+
+    public class func disableAutoresizingMasksInViews(views: UIView...) {
+        for view in views {
+            view.setTranslatesAutoresizingMaskIntoConstraints(false)
+        }
+    }
+    
+}
