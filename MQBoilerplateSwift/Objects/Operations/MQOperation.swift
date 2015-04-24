bbbe749449bbaa62229f0a678e7cc32fd9aa5e44
Matt Quiros <mattquiros@gmail.com>
2015-04-24 16:53:04 +0800

    Changes to MQOperation

--- MQBoilerplateSwift/Objects/Operations/MQOperation.swift
+++ MQBoilerplateSwift/Objects/Operations/MQOperation.swift
@@ -123,7 +123,6 @@ public class MQOperation : NSOperation {
                 self.errorPresenter!.presentViewController(alertController, animated: true, completion: nil)
             })
         }
-
     }
     
 }
