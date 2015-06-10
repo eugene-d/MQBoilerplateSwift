67d2a6f7673bf8f7828728fdaf4ea6150a80d294
Matt Quiros <mattquiros@gmail.com>
2015-06-11 01:21:00 +0800

    Added custom button classes

---
+++ MQBoilerplateSwift/Views/MQLabelButton.swift
@@ -0,0 +1,24 @@
+//
+//  MQLabelButton.swift
+//  MQBoilerplateSwift
+//
+//  Created by Matt Quiros on 6/8/15.
+//  Copyright (c) 2015 Matt Quiros. All rights reserved.
+//
+
+import UIKit
+
+public class MQLabelButton: MQButton {
+    
+    public lazy var label: UILabel = UILabel()
+    
+    public override init() {
+        super.init()
+        self.customView.addSubviewAndFill(self.label)
+    }
+
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
