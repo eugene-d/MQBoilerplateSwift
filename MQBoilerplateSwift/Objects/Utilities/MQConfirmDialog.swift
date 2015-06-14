d5609700c6d4f63d26f0a1ebab1de4ce9cc94dff
Matt Quiros <mattquiros@gmail.com>
2015-06-14 14:48:10 +0800

    Added class func for confirm dialog, changed version number

--- MQBoilerplateSwift/Objects/Utilities/MQConfirmDialog.swift
+++ MQBoilerplateSwift/Objects/Utilities/MQConfirmDialog.swift
@@ -10,11 +10,19 @@ import Foundation
 
 public class MQConfirmDialog {
     
+    @availability(*, deprecated=1.10)
     public var title: String
+    
+    @availability(*, deprecated=1.10)
     public var message: String
+    
+    @availability(*, deprecated=1.10)
     public var confirmButtonTitle: String
+    
+    @availability(*, deprecated=1.10)
     public var cancelButtonTitle: String
     
+    @availability(*, deprecated=1.10)
     public init() {
         self.title = "Confirm"
         self.message = "Are you sure?"
@@ -22,6 +30,7 @@ public class MQConfirmDialog {
         self.cancelButtonTitle = "Cancel"
     }
     
+    @availability(*, deprecated=1.10)
     public func showInPresenter(presenter: UIViewController, confirmAction someAction: (() -> Void)?) {
         let alertController = UIAlertController(title: self.title, message: self.message, preferredStyle: .Alert)
         
@@ -41,6 +50,32 @@ public class MQConfirmDialog {
         alertController.addAction(cancelAction)
         
         presenter.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    public class func showInPresenter(presenter: UIViewController,
+        title: String = "Confirm",
+        message: String = "Are you sure?",
+        confirmButtonTitle: String = "Yes",
+        cancelButtonTitle: String = "Cancel",
+        confirmAction someAction: (() -> ())?) {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+            
+            let confirmButtonAction = UIAlertAction(title: confirmButtonTitle,
+                style: .Default) { _ in
+                    if let confirmAction = someAction {
+                        confirmAction()
+                    }
+                    alertController.dismissViewControllerAnimated(true, completion: nil)
+            }
+            alertController.addAction(confirmButtonAction)
+            
+            let cancelAction = UIAlertAction(title: cancelButtonTitle,
+                style: UIAlertActionStyle.Cancel) { _ in
+                    alertController.dismissViewControllerAnimated(true, completion: nil)
+            }
+            alertController.addAction(cancelAction)
+            
+            presenter.presentViewController(alertController, animated: true, completion: nil)
     }
     
 }
