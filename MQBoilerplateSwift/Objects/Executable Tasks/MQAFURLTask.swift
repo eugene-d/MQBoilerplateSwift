f97ab6afec8c56975b2dc7bf4ce532d8cb288720
Matt Quiros <mattquiros@gmail.com>
2015-06-23 12:20:48 +0800

    Added Alamofire without adding git submodule, added base data task objects

--- MQBoilerplateSwift/Objects/Executable Tasks/MQAFURLTask.swift
+++ MQBoilerplateSwift/Objects/Executable Tasks/MQAFURLTask.swift
@@ -7,10 +7,55 @@
 //
 
 import Foundation
-//import Alamofire
+import Alamofire
 
-public class MQAFURLTask {
+public class MQAFURLTask: MQExecutableTask {
     
+    public var type: MQExecutableTaskType {
+        return .Default
+    }
     
+    public var startBlock: (() -> Void)?
+    public var returnBlock: (() -> Void)?
+    public var failureBlock: ((NSError) -> Void)?
+    public var successBlock: ((Any?) -> Void)?
+    public var finishBlock: (() -> Void)?
+    
+    public var result: Any?
+    public var error: NSError?
+    
+    public var manager: Alamofire.Manager
+    var method: Alamofire.Method
+    public var URL: String
+    var parameters: [String : AnyObject]?
+    var parameterEncoding: Alamofire.ParameterEncoding
+    
+    public init(manager: Alamofire.Manager, method: Alamofire.Method, URL: String, parameters: [String : AnyObject]?, parameterEncoding: Alamofire.ParameterEncoding) {
+        self.manager = manager
+        self.method = method
+        self.URL = URL
+        self.parameters = parameters
+        self.parameterEncoding = parameterEncoding
+    }
+    
+    public func execute() {
+        self.performSequence()
+    }
+    
+    public func performSequence() {
+        
+    }
+    
+    public func computeResult() {
+        
+    }
+    
+    public func handleResponse(someRequest: NSURLRequest?, _ someResponse: NSURLResponse?, _ someObject: AnyObject?, _ someError: NSError?) {
+        
+    }
+    
+    public func buildResult(object: Any?) -> Any? {
+        return nil
+    }
     
 }
