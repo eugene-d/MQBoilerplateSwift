1b489f51127fe4609acf9a838bd71bd48e5a9c27
Matt Quiros <mattquiros@gmail.com>
2015-06-23 08:45:21 +0800

    Added url task based on AF

---
+++ MQBoilerplateSwift/Objects/Executable Tasks/MQAFURLTask.swift
@@ -0,0 +1,16 @@
+//
+//  MQAFURLTask.swift
+//  MQBoilerplateSwift
+//
+//  Created by Matt Quiros on 6/23/15.
+//  Copyright © 2015 Matt Quiros. All rights reserved.
+//
+
+import Foundation
+//import Alamofire
+
+public class MQAFURLTask {
+    
+    
+    
+}
