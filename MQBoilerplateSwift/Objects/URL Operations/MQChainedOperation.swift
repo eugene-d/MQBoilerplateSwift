c49328abd390410abd82f93f1e009785ece34688
Matt Quiros <mattquiros@gmail.com>
2015-07-29 15:40:38 +0800

    Replaced chained operations to accept conditions

--- MQBoilerplateSwift/Objects/URL Operations/MQChainedOperation.swift
+++ MQBoilerplateSwift/Objects/URL Operations/MQChainedOperation.swift
@@ -20,10 +20,11 @@ regardless of which operation in the chain terminates the chain.
 public class MQChainedOperation: MQAsynchronousOperation {
     
     var operationQueue = NSOperationQueue()
-    var operations: [MQOperation]
     
-    public init(operations: MQOperation ...) {
-        self.operations = operations
+    var items: [Any]
+    
+    public init(_ items: Any ...) {
+        self.items = items
     }
     
     public override func main() {
@@ -35,44 +36,26 @@ public class MQChainedOperation: MQAsynchronousOperation {
             return
         }
         
-        for i in 0..<self.operations.count {
-            let currentOperation = self.operations[i]
-            currentOperation.returnBlock = nil
-            currentOperation.finishBlock = nil
-            if self.failureBlock != nil {
-                currentOperation.failureBlock = nil
+        // This loop builds the chain.
+        
+        for i in 0..<self.items.count {
+            guard self.items[i] is MQOperation || self.items[i] is (MQOperation, () -> Bool) else {
+                fatalError("Found unrecognised type \(self.items[i]) in the operation chain")
             }
+            
+            let currentItem = self.items[i]
+            
+            self.overrideCallbackBlocks(currentItem)
             
             if i == 0 {
-                currentOperation.startBlock = self.startBlock
+                let operation = operationFromItem(currentItem)
+                operation.startBlock = self.startBlock
             }
             
-            // Configure the success block chain.
-            // If there are more operations in the chain, set the successBlock to fire its
-            // custom logic and then execute the next operation.
-            // Otherwise, run the returnBlock, then the success logic,
-            // then the successBlock for the entire chain, and then the finishBlock.
-            let customSuccessBlock = currentOperation.successBlock
-            if i + 1 < self.operations.count {
-                currentOperation.successBlock = {[unowned self] result in
-                    customSuccessBlock?(result)
-                    let nextOperation = self.operations[i + 1]
-                    self.operationQueue.addOperation(nextOperation)
-                }
+            if i + 1 < self.items.count {
+                self.link(currentItem, nextItem: self.items[i + 1])
             } else {
-                currentOperation.successBlock = {[unowned self] result in
-                    self.runReturnBlock()
-                    customSuccessBlock?(result)
-                    self.successBlock?(result)
-                    self.runFinishBlock()
-                }
-            }
-            
-            // Override the failureBlock.
-            currentOperation.failureBlock = {[unowned self] error in
-                self.runReturnBlock()
-                self.runFailureBlockWithError(error)
-                self.runFinishBlock()
+                self.concludeWithItem(currentItem)
             }
             
             if self.cancelled {
@@ -80,12 +63,65 @@ public class MQChainedOperation: MQAsynchronousOperation {
             }
         }
         
-        if let firstOperation = self.operations.first {
-            self.operationQueue.addOperation(firstOperation)
+        if let firstItem = self.items.first {
+            let operation = operationFromItem(firstItem)
+            self.operationQueue.addOperation(operation)
             
             // Exit main() only when all operations in the chain are finished.
             self.operationQueue.waitUntilAllOperationsAreFinished()
         }
+        
+        /*for i in 0..<self.operations.count {
+        let currentOperation = self.operations[i]
+        currentOperation.returnBlock = nil
+        currentOperation.finishBlock = nil
+        if self.failureBlock != nil {
+        currentOperation.failureBlock = nil
+        }
+        
+        if i == 0 {
+        currentOperation.startBlock = self.startBlock
+        }
+        
+        // Configure the success block chain.
+        // If there are more operations in the chain, set the successBlock to fire its
+        // custom logic and then execute the next operation.
+        // Otherwise, run the returnBlock, then the success logic,
+        // then the successBlock for the entire chain, and then the finishBlock.
+        let customSuccessBlock = currentOperation.successBlock
+        if i + 1 < self.operations.count {
+        currentOperation.successBlock = {[unowned self] result in
+        customSuccessBlock?(result)
+        let nextOperation = self.operations[i + 1]
+        self.operationQueue.addOperation(nextOperation)
+        }
+        } else {
+        currentOperation.successBlock = {[unowned self] result in
+        self.runReturnBlock()
+        customSuccessBlock?(result)
+        self.successBlock?(result)
+        self.runFinishBlock()
+        }
+        }
+        
+        // Override the failureBlock.
+        currentOperation.failureBlock = {[unowned self] error in
+        self.runReturnBlock()
+        self.runFailureBlockWithError(error)
+        self.runFinishBlock()
+        }
+        
+        if self.cancelled {
+        return
+        }
+        }
+        
+        if let firstOperation = self.operations.first {
+        self.operationQueue.addOperation(firstOperation)
+        
+        // Exit main() only when all operations in the chain are finished.
+        self.operationQueue.waitUntilAllOperationsAreFinished()
+        }*/
     }
     
     public override func cancel() {
@@ -95,4 +131,76 @@ public class MQChainedOperation: MQAsynchronousOperation {
         super.cancel()
     }
     
+    /**
+    Disregards the callback blocks of the individual operations in the chain to adopt the
+    chain's callback blocks instead.
+    */
+    func overrideCallbackBlocks(item: Any) {
+        let operation = operationFromItem(item)
+        operation.returnBlock = nil
+        operation.finishBlock = nil
+        operation.failureBlock = {[unowned self] error in
+            self.runReturnBlock()
+            self.runFailureBlockWithError(error)
+            self.runFinishBlock()
+        }
+    }
+    
+    /**
+    Links two items in the chain. If the second argument is nil, it is assumed that the first argument
+    is the end of the chain and its `successBlock` will contain logic to wrap up the chain instead of
+    chaining to another operation.
+    */
+    func link(item: Any, nextItem: Any) {
+        let operation = operationFromItem(item)
+        let nextOperation = operationFromItem(nextItem)
+        let condition = conditionFromItem(nextItem)
+        self.link(op1: operation, op2: nextOperation, condition: condition)
+    }
+    
+    func concludeWithItem(item: Any) {
+        let operation = operationFromItem(item)
+        let customSuccessBlock = operation.successBlock
+        operation.successBlock = {[unowned self] result in
+            self.runReturnBlock()
+            customSuccessBlock?(result)
+            self.successBlock?(result)
+            self.runFinishBlock()
+        }
+    }
+    
+    /**
+    Links two operations. If a condition is provided, the second operation is chained
+    only if the condition evaluates to true.
+    */
+    func link(op1 op1: MQOperation, op2: MQOperation, condition: (() -> Bool)?) {
+        let customSuccessBlock = op1.successBlock
+        op1.successBlock = {[unowned self] result in
+            customSuccessBlock?(result)
+            if let condition = condition {
+                if condition() == true {
+                    self.operationQueue.addOperation(op2)
+                }
+            } else {
+                self.operationQueue.addOperation(op2)
+            }
+        }
+    }
+    
+}
+
+private func operationFromItem(item: Any) -> MQOperation {
+    if let operation = item as? MQOperation {
+        return operation
+    } else if let (operation, _) = item as? (MQOperation, () -> Bool) {
+        return operation
+    }
+    fatalError("Unrecognized type: \(item)")
+}
+
+private func conditionFromItem(item: Any) -> (() -> Bool)? {
+    if let (_, condition) = item as? (MQOperation, () -> Bool) {
+        return condition
+    }
+    return nil
 }
--- MQBoilerplateSwift/Objects/URL Operations/MQURLOperation.swift
+++ MQBoilerplateSwift/Objects/URL Operations/MQURLOperation.swift
@@ -22,25 +22,95 @@ public class MQURLOperation: MQAsynchronousOperation {
         case CONNECT = "CONNECT"
     }
     
+    public enum ContentType {
+        /**
+        Sets the `Content-Type` HTTP header to `application/json`.
+        */
+        case JSON
+        
+        /**
+        Sets the `Content-Type` HTTP header to `multipart/form-data; boundary=` plus
+        the value of the `formDataBoundary` property. You must therefore set the `formDataBoundary`
+        property if you choose this content type.
+        */
+        case MultipartFormData
+    }
+    
+    /**
+    The `NSURLSession` object which creates the URL tasks. Ideally, you create subclasses of
+    `MQURLOperation` that share a singleton `NSURLSession` object, and you simply pass the singleton
+    to the `MQURLOperation` initializer.
+    */
     public var session: NSURLSession
     public var method: MQURLOperation.Method
     public var URL: String
+    
+    /**
+    The `Content-Type` HTTP header field.
+    */
+    public var contentType: ContentType
+    
     public var parameters: [String : AnyObject]?
+    
+    /**
+    The boundary string for multipart form data requests.
+    */
+    public var formDataBoundary: String!
     
     private var task: NSURLSessionDataTask!
     
     // MARK -
     
-    public init(session: NSURLSession, method: MQURLOperation.Method, URL: String, parameters: [String : AnyObject]?) {
-        self.session = session
-        self.method = method
-        self.URL = URL
-        self.parameters = parameters
+    public init(session: NSURLSession,
+        method: MQURLOperation.Method,
+        URL: String,
+        contentType: ContentType,
+        parameters: [String : AnyObject]? = nil) {
+            self.session = session
+            self.method = method
+            self.URL = URL
+            self.contentType = contentType
+            self.parameters = parameters
     }
     
-    public func buildRequest() -> NSURLRequest {
-        let request = NSURLRequest(URL: NSURL(string: self.URL)!)
+    /**
+    Builds the URL request object on which the URL task will be based. To set your own HTTP headers,
+    override this function and edit the request object returned by the `super` implementation.
+    */
+    public func createRequest() -> NSMutableURLRequest {
+        let URLString = self.URL.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLFragmentAllowedCharacterSet())!
+        let request = NSMutableURLRequest(URL: NSURL(string: URLString)!)
+        request.HTTPMethod = self.method.rawValue
+        
+        switch self.contentType {
+        case .JSON:
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            do {
+                if let parameters = self.parameters {
+                    let HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: [])
+                    request.HTTPBody = HTTPBody
+                }
+            } catch {
+                fatalError("Cannot encode JSON parameters")
+            }
+            
+        case .MultipartFormData:
+            guard let boundary = self.formDataBoundary else {
+                fatalError("Initialised a multipart form data request without specifying formDataBoundary")
+            }
+            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            request.HTTPBody = self.createMultipartFormData()
+        }
+        
         return request
+    }
+    
+    /**
+    Override point for building the HTTP body of a multipart form data request.
+    If you set the `contentType` to `.MultipartFormData`, you must override this function.
+    */
+    public func createMultipartFormData() -> NSMutableData {
+        fatalError("Did not override: \(__FUNCTION__)")
     }
     
     public override func main() {
@@ -56,7 +126,7 @@ public class MQURLOperation: MQAsynchronousOperation {
             return
         }
         
-        let request = self.buildRequest()
+        let request = self.createRequest()
         self.task = self.session.dataTaskWithRequest(request) {[unowned self] (data, response, error) in
             self.handleResponse(response, data, error)
         }
@@ -67,7 +137,7 @@ public class MQURLOperation: MQAsynchronousOperation {
     Handles the response returned by the server. You should override this method in a base
     `MQURLOperation` class so that all its children have a uniform behavior of processing an API's response.
     
-    **IMPORTANT** You must call `super.handleResponse()` at the very end of your override to 
+    **IMPORTANT** You must call `super.handleResponse()` at the very end of your override to
     make sure that the `NSOperation` state flags are correctly updated.
     */
     public func handleResponse(someResponse: NSURLResponse?,
